3db947726d358e413552b5438fd12a607c935696
Sabrine <bs.sabrina13@gmail.com>
2021-07-03 07:12:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+import { Link, Switch, Route } from 'react-router-dom'
+
+
+function App() {
+  const testProps = "test props"
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="/users" render={(props) =>
+          <div>
+            <Users {...props} />
+            <About {...props} testProps={testProps} />
+          </div>
+        } />
+
+      </Switch>
+    </div>
+
+  );
+}
+
+export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
